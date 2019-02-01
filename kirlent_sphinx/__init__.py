c0629db409ac1614ec18463b21c906506daddb89
H. Turgut Uyar <uyar@tekir.org>
2019-02-01 10:51:18 +0300

    docs: Update init module documentation

--- kirlent_sphinx/__init__.py
+++ kirlent_sphinx/__init__.py
@@ -10,7 +10,7 @@ _template_path = Path(__file__).parent.joinpath("templates")
 
 
 def get_path():
-    """Get the template path.
+    """Get the base path for templates.
 
     :sig: () -> str
     :return: Base path for the templates of this extension.
@@ -19,11 +19,15 @@ def get_path():
 
 
 def setup(app):
-    """Add the directives and themes of this extension to Sphinx.
+    """Register this extension with Sphinx.
 
-    :sig: (sphinx.application.Sphinx) -> None
-    :param app: Sphinx application to setup.
+    This will add the directives and themes of this extension to Sphinx.
+
+    :sig: (sphinx.application.Sphinx) -> Dict[str, str]
+    :param app: Sphinx application to register this extension with.
+    :return: Information about this extension.
     """
+    tables.setup(app)
     slides.setup(app)
     app.add_html_theme("kirlent", str(Path(_template_path, "kirlent")))
-    tables.setup(app)
+    return {"version": __version__}
