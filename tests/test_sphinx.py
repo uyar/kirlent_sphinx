136a1d9613126fa19787142ae77ec99a363a9e70
H. Turgut Uyar <uyar@tekir.org>
2019-02-05 17:31:51 +0300

    test: Simplify content generation for test files

--- tests/test_sphinx.py
+++ tests/test_sphinx.py
@@ -11,14 +11,38 @@ from kirlent_sphinx import __version__
 
 SOURCE = "/dev/shm/kirlent_sphinx"
 
+CONF_PY_CONTENT = """
+extensions = ["kirlent_sphinx"]
+html_theme = "kirlent"
+html_use_index = False
+master_doc = "index"
+"""
+
+SLIDE_CONTENT_1 = """
+.. slide:: Slide title
+
+   - item 1
+"""
+
+
+TABLE_CONTENT_1 = """
+.. slide:: Slide title
+
+   .. table::
+      :widths: 1 2
+      
+      +---+---+
+      | a | b |
+      +---+---+
+"""
+
 
 @fixture(scope="session", autouse=True)
 def setup_sphinx():
     """A skeleton Sphinx project."""
     Path(SOURCE).mkdir(parents=True, exist_ok=True)
     conf_py = Path(SOURCE, "conf.py")
-    conf = """extensions=["kirlent_sphinx"]\nhtml_theme="kirlent"\nhtml_use_index=False\nmaster_doc="index"\n"""
-    conf_py.write_text(conf)
+    conf_py.write_text(CONF_PY_CONTENT)
     yield
     rmtree(SOURCE)
 
@@ -28,14 +52,12 @@ def test_version():
 
 
 def test_slide_directive_should_be_available():
-    content = ".. slide:: Slide Title\n\n   - item 1\n"
-    Path(SOURCE, "index.rst").write_text(content)
+    Path(SOURCE, "index.rst").write_text(SLIDE_CONTENT_1)
     subprocess.call(["sphinx-build", "-b", "html", SOURCE, SOURCE + "/_build"])
     assert Path(SOURCE, "_build", "index.html").exists()
 
 
 def test_table_directive_should_be_available():
-    content = ".. slide:: Slide Title\n\n   .. table::      :widths: 1\n\n      +---+\n      | a |\n      +---+\n"
-    Path(SOURCE, "index.rst").write_text(content)
+    Path(SOURCE, "index.rst").write_text(TABLE_CONTENT_1)
     subprocess.call(["sphinx-build", "-b", "html", SOURCE, SOURCE + "/_build"])
     assert Path(SOURCE, "_build", "index.html").exists()
