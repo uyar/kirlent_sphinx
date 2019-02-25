0b3099622aaf16259a027ba85c558c20616e7ae9
H. Turgut Uyar <uyar@tekir.org>
2019-02-25 17:07:44 +0300

    refactor: Change default font to Oxygen

--- kirlent_sphinx/templates/kirlent/tailwind.js
+++ kirlent_sphinx/templates/kirlent/tailwind.js
@@ -197,17 +197,7 @@ module.exports = {
 
   fonts: {
     'sans': [
-      'system-ui',
-      'BlinkMacSystemFont',
-      '-apple-system',
-      'Segoe UI',
-      'Roboto',
       'Oxygen',
-      'Ubuntu',
-      'Cantarell',
-      'Fira Sans',
-      'Droid Sans',
-      'Helvetica Neue',
       'sans-serif',
     ],
     'serif': [
