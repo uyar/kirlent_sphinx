a67403ec038d57a4cb49c3a9d3f7e4d8cbc77925
H. Turgut Uyar <uyar@tekir.org>
2019-03-15 10:42:35 +0300

    setup: Bump version for development

--- kirlent_sphinx/__init__.py
+++ kirlent_sphinx/__init__.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from . import slides, tables
 
 
-__version__ = "0.2.2"  # sig: str
+__version__ = "0.2.3.dev.0"  # sig: str
 
 
 _template_path = Path(Path(__file__).parent, "templates")
